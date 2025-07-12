fe5a8b9fafffe32f8e10b4437aa8f112af921f51
Jerson Cruz <jerson.fabricio.cruz.10@gmail.com>
2025-07-11 19:11:47 -0500

    Cambio a PostgreSQL para producción en VPS

--- tienda/settings.py
+++ tienda/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=^pi1*4mk3w!g5o#y5py(u6yo7j_d1ubze$64e!o%mm2v55lif'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'tienda.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TIenda_db',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
